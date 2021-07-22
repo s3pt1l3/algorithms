830f0217071cbe512a2bc2268c2b90d41dfe50f8
Semyon Stepanov <45712837+s3pt1l3@users.noreply.github.com>
2021-07-22 13:05:09 +0300

    Delete gcd.py

--- gcd/gcd.py
+++
@@ -1,8 +0,0 @@
-def gcd(a, b):
-    while a != 0 and b != 0:
-        if a > b:
-            a = a % b
-        else:
-            b = b % a
-    if not (a == 0 and b == 0):
-        return max(a, b)
