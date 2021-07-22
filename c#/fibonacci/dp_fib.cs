ed099eb044a86807ee24c3698f94f77936b8e8a4
Semyon Stepanov <45712837+s3pt1l3@users.noreply.github.com>
2021-07-22 14:26:40 +0300

    Dynamic programming fibonacci

---
+++ c#/fibonacci/dp_fib.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace json_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(recursiveFib(4));
+            Console.ReadKey();
+        }
+        static int recursiveFib(int pos)
+        {
+            if (pos < 2)
+            {
+                return pos;
+            }
+            int[] nums = new int[pos + 1];
+            nums[0] = 0;
+            nums[1] = 1;
+            for (int i = 2; i < pos + 1; ++i)
+            {
+                nums[i] = nums[i - 2] + nums[i - 1];
+            }
+            return nums[pos];
+        }
+    }
+}
